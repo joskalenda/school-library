a1bb038c4638227f079c3ad858ad00397216f5de
joskalenda <joskalenda3@gmail.com>
2022-04-08 23:28:37 +0200

    Crate class classrom, add inner method

---
+++ classroom.rb
@@ -0,0 +1,13 @@
+class Classroom
+  def initialize(label)
+    @label = label
+    @students = []
+    attr_accessor :label
+    attr_reader :students
+  end
+
+  def add_student(student)
+    @students.push(student)
+    student.classroom = self
+  end
+end
