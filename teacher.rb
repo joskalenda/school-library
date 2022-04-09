dcb21fca42fae7533528728f6c8687542074eb0d
joskalenda <joskalenda3@gmail.com>
2022-04-09 15:05:08 +0200

    Fix permission issue

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require './person'
 
 class Teacher < Person
   def initialize(specialization, age, name = 'unknown', parent_permission: true)
-    super(name, age, parent_permission)
+    super(name, age, parent_permission: parent_permission)
     @specialization = specialization
   end
 
