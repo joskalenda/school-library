c76c7e83d79135f4cdee7a9abfe98aecceb21980
joskalenda <joskalenda3@gmail.com>
2022-04-15 23:11:12 +0200

    Add unit test for book class

---
+++ spec/book_spec.rb
@@ -0,0 +1,18 @@
+require './book'
+
+describe Book do
+
+  it 'Book class instance' do
+    title = 'Ruby made simple'
+    author = 'Topaz'
+    @book = Book.new(title, author)
+    expect(@book).to be_instance_of(Book)
+  end
+
+  it 'Check if book attribute match' do
+    title = 'Na relax'
+    author = 'Koko G'
+    book = Book.new(title, author)
+    expect(book).to have_attributes(title: title, author: author)
+  end
+end
